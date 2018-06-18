ec135c79a0b37f5dc1d8637c3877a655ea6bf310
Travis Hoppe <travis.hoppe@gmail.com>
2018-06-18 00:37:29 -0400

    Check inputs

--- grid.py
+++ grid.py
@@ -5,10 +5,6 @@ import numpy as np
 from lapjv import lapjv
 from sklearn.manifold import TSNE
 from scipy.spatial.distance import cdist
-import cv2
-
-from tqdm import tqdm
-import random
 
 def generate_tsne(activations, perplexity=50, tsne_iter=5000):
     tsne = TSNE(
@@ -22,21 +18,21 @@ def generate_tsne(activations, perplexity=50, tsne_iter=5000):
     X_2d /= X_2d.max(axis=0)
     return X_2d
 
-def fit_to_grid(img_collection, X_2d, out_dim, out_res=224):
+def fit_to_grid(IMG, X_2d, n, out_res=224):
     grid = np.dstack(np.meshgrid(
-        np.linspace(0, 1, out_dim),
-        np.linspace(0, 1, out_dim))).reshape(-1, 2)
+        np.linspace(0, 1, n),
+        np.linspace(0, 1, n))).reshape(-1, 2)
     
     cost_matrix = cdist(grid, X_2d, "sqeuclidean").astype(np.float32)
     cost_matrix = cost_matrix * (100000 / cost_matrix.max())
     row_asses, col_asses, _ = lapjv(cost_matrix)
     
     grid_jv = grid[col_asses]
-    out = np.ones((out_dim*out_res, out_dim*out_res, 3))
+    out = np.ones((n*out_res, n*out_res, 3))
 
-    for pos, img in zip(grid_jv, img_collection):
-        h_range = int(np.floor(pos[0]* (out_dim - 1) * out_res))
-        w_range = int(np.floor(pos[1]* (out_dim - 1) * out_res))
+    for pos, img in zip(grid_jv, IMG):
+        h_range = int(np.floor(pos[0]* (n - 1) * out_res))
+        w_range = int(np.floor(pos[1]* (n - 1) * out_res))
         out[
             h_range:h_range + out_res,
             w_range:w_range + out_res]  = img
--- render.py
+++ render.py
@@ -20,8 +20,12 @@ from docopt import docopt
 dargs = docopt(__doc__)
 
 total_images = int(dargs["<n_images>"])
-max_image_row_size = 20
+square_n = int(np.sqrt(total_images))
 
+if square_n**2 != total_images:
+    raise ValueError(f"<n_images={total_images}> must be a square number!")
+
+max_image_row_size = 20
 VGG_size = 224
 
 name = dargs["<term>"]
@@ -104,42 +108,43 @@ def compute_activations(f0):
     np.savetxt(f1, ax)
 
 
-# Create any missing directories
-for d in [subimage_dest, figure_dest, activations_dest]:
-    if not os.path.exists(d):
-        os.system(f'mkdir -p "{d}"')
+if __name__ == "__main__":
 
-F_IN = set(sorted(glob.glob(os.path.join(load_dest, '*'))))
+    # Create any missing directories
+    for d in [subimage_dest, figure_dest, activations_dest]:
+        if not os.path.exists(d):
+            os.system(f'mkdir -p "{d}"')
 
-# Remove all zero-byte files
-for f in list(F_IN):
-    if os.stat(f).st_size==0:
-        print(f"Removing zero-byte file {f}")
-        os.remove(f)
-        F_IN.remove(f)
+    F_IN = set(sorted(glob.glob(os.path.join(load_dest, '*'))))
 
-for f0 in tqdm(F_IN):
-    resize_and_crop(f0)
+    # Remove all zero-byte files
+    for f in list(F_IN):
+        if os.stat(f).st_size==0:
+            print(f"Removing zero-byte file {f}")
+            os.remove(f)
+            F_IN.remove(f)
 
-F_IN = set(sorted(glob.glob(os.path.join(subimage_dest, '*'))))
-for f0 in tqdm(F_IN):
-    compute_activations(f0)
+    for f0 in tqdm(F_IN):
+        resize_and_crop(f0)
 
-# Check to make sure we have enough images
-F_IN = set(sorted(glob.glob(os.path.join(activations_dest, '*'))))
-if len(F_IN) < total_images:
-    msg = f"Not enough images for {name}, {len(F_IN)}/{total_images}"
-    raise ValueError(msg)
+    F_IN = set(sorted(glob.glob(os.path.join(subimage_dest, '*'))))
+    for f0 in tqdm(F_IN):
+        compute_activations(f0)
 
+    # Check to make sure we have enough images
+    F_IN = set(sorted(glob.glob(os.path.join(activations_dest, '*'))))
+    if len(F_IN) < total_images:
+        msg = f"Not enough images for {name}, {len(F_IN)}/{total_images}"
+        raise ValueError(msg)
+    
+    IMG, ACT = load_image_data()
 
-IMG, ACT = load_image_data()
+    from grid import generate_tsne, fit_to_grid
+    X = generate_tsne(ACT)
+    img = fit_to_grid(IMG, X, square_n)
 
-from grid import generate_tsne, fit_to_grid
-X = generate_tsne(ACT)
-img = fit_to_grid(IMG, X, int(np.sqrt(total_images)))
+    f_img_save = os.path.join(figure_dest, f"{name}.jpg")
+    cv2.imwrite(f_img_save, img)
+    print (f"Saved output image to {f_img_save}")
 
-f_img_save = os.path.join(figure_dest, f"{name}.jpg")
-cv2.imwrite(f_img_save, img)
-print (f"Saved output image to {f_img_save}")
-
-os.system(f'eog "figures/{name}.jpg"')
+    os.system(f'eog "figures/{name}.jpg"')
